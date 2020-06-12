dde7aad90422783b98ae6c60351769569a0c31c1
PJZ9n <pjz9n@yahoo.co.jp>
2020-06-12 10:46:31 +0900

    音楽再生

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-
+const YTDL = require("ytdl-core");
 const DISCORD = require("discord.js");
 const CLIENT = new DISCORD.Client();
 
@@ -11,6 +11,21 @@ CLIENT.on("message", message => {
     if (message.content === "hi") {
         console.log("reply!");
         message.reply("Hello!");
+    }
+});
+
+CLIENT.on("message", async message => {
+    if (!message.guild) return;
+    if (message.content === "vc") {
+        if (message.member.voice.channel) {
+            const CONNECTION = await message.member.voice.channel.join();
+            let y = YTDL("https://www.youtube.com/watch?v=9lVPAWLWtWc", {
+                filter: "audioonly"
+            });
+            CONNECTION.play(y);
+        } else {
+            message.reply("VCに入ってください");
+        }
     }
 });
 
